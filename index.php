9dc55217ba65460b51edaeb7ad58b5d001baee7e
bagusaliakbar <bagusaliakbar@unsub.ac.id>
2026-07-26 07:52:42 +0700

    chore: update public path to current directory in index.php

--- index.php
+++ index.php
@@ -17,4 +17,7 @@ require __DIR__.'/vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/bootstrap/app.php';
 
+// Set public path to this directory
+$app->usePublicPath(__DIR__);
+
 $app->handleRequest(Request::capture());
