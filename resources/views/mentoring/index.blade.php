d218a44c42d99c75a6bb12fc208710f2a6cc8390
bagusaliakbar <bagusaliakbar@unsub.ac.id>
2026-08-30 14:20:13 +0700

    fix: tutup directive @can('update') yang terlewat pada tombol ubah di mentoring index

--- resources/views/mentoring/index.blade.php
+++ resources/views/mentoring/index.blade.php
@@ -434,7 +434,8 @@
                                                     <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                     <span>Ubah</span>
                                                 </a>
-                                                                                        @can('delete', $session)
+                                            @endcan
+                                            @can('delete', $session)
                                                 <button type="button" 
                                                         @click="openCancelModalFromEl($el)"
                                                         data-session-id="{{ $session->id }}"
